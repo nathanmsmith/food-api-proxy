97e65127bd5e5d183d127bc9b075f1f8c0a6e830
Nathan Smith <nathan@nathansmith.io>
2018-11-27 22:01:52 -0800

    not yarn error file

--- index.js
+++ index.js
@@ -35,4 +35,31 @@ app.get('/ingredient-search', async (req, res, next) => {
   }
 })
 
+app.get('/ingredient-search', async (req, res, next) => {
+  try {
+    const ingredient = req.query.q
+    const apiResult = await fetch(
+      `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/ingredients/autocomplete?query=${ingredient}&number=10`,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Mashape-Key': process.env.FOOD_API_KEY,
+          'X-Mashape-Host':
+            'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
+        },
+      }
+    )
+    const data = await apiResult.json()
+    const dataWithImageURLs = data.map(ingredient => {
+      ingredient.image = `https://spoonacular.com/cdn/ingredients_250x250/${
+        ingredient.image
+      }`
+      return ingredient
+    })
+    res.json(dataWithImageURLs)
+  } catch (error) {
+    next(error)
+  }
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
