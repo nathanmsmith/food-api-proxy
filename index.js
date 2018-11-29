3cf5114290e568cc0c1c6d1bb197e13f815b09d6
etliu <eric.t.liu15@gmail.com>
2018-11-29 13:04:46 -0800

    add ingredient list data

--- index.js
+++ index.js
@@ -99,6 +99,7 @@ app.get('/get-recipe-info', async (req, res, next) => {
     const data = {
       ...summaryData,
       instructions: instructionsData,
+      ingredients: infoData.extendedIngredients.map(e => e.name),
       image: infoData.image,
       prepTime: infoData.readyInMinutes,
       servings: infoData.servings,
